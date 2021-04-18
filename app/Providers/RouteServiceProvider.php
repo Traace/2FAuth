31f4a5936144ff2832841f4b4b93566992639a21
Traace <xxtraacexx@hotmail.de>
2021-04-18 10:56:17 +0200

    Forcing HTTPS scheme
    Forcing this scheme makes HTTPS reverse proxies happy.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,8 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        
         parent::boot();
     }
 
